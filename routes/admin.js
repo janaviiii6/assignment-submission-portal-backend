bcd4012dc529d60becb764cb4c18a6ade5492cae
JanaviK <kharatjanavi16@gmail.com>
2024-11-17 22:38:33 +0530

    Added route to accept the assignment

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { viewAllAssignments } = require("../controllers/admin");
+const { viewAllAssignments, acceptAssignment } = require("../controllers/admin");
 
+
+//Route for fetching all the assignments
 router.get("/assignments",viewAllAssignments);
+
+//Route to accept the assignment
+router.post("/assignment/:id/accept",acceptAssignment);
 
 module.exports = router;
