c133343525eee7fe6f23ee4cc22518b09938a357
JanaviK <kharatjanavi16@gmail.com>
2024-11-17 17:33:51 +0530

    Added route to fetch all admins

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { handleUserRegister, handleUserLogin } = require("../controllers/user");
+const { handleUserRegister, handleUserLogin, getAllAdmins } = require("../controllers/user");
 const router = express.Router();
+const { authenticateJWT } = require("../middlewares/auth");
 
 router.post("/register",handleUserRegister);
+
 router.post("/login",handleUserLogin);
+
+router.get("/admins",authenticateJWT ,getAllAdmins);
+
 
 module.exports = router;
 
