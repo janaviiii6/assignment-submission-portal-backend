08e14e846d574ac31bf9160870f7464cfd48b981
JanaviK <kharatjanavi16@gmail.com>
2024-11-17 17:36:52 +0530

    Added getAllAdmins function

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
+
+//Authentication
+
 const handleUserRegister = async (req,res) => {
     const { name, email, password, role } = req.body;
     try{
@@ -64,4 +67,24 @@ const handleUserLogin = async (req,res) => {
 };
 
 
-module.exports = { handleUserRegister, handleUserLogin };
+// normal routes function
+const getAllAdmins = async (req,res) => {
+    try{
+        const admins = await User.find({ role: 'admin' }).select('name email');
+
+        if(admins.length === 0)
+            return res.status(404).json({ message: "No admins found." });
+
+        res.status(200).json({
+            message: "Admins fetched successfully!",
+            admins
+        });
+    }
+    catch(err) {
+        console.error("Error fetching admins: ", err);
+        res.status(500).json({ message: "Server error" });  
+
+    }
+}
+
+module.exports = { handleUserRegister, handleUserLogin, getAllAdmins };
