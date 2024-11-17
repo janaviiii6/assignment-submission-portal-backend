a4c0c2336d4e3801113ea493d2a30f7f1e234410
JanaviK <kharatjanavi16@gmail.com>
2024-11-17 15:37:42 +0530

    Added login route for user

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { handleUserRegister } = require("../controllers/user");
+const { handleUserRegister, handleUserLogin } = require("../controllers/user");
 const router = express.Router();
 
 router.post("/register",handleUserRegister);
+router.post("/login",handleUserLogin);
 
 module.exports = router;
 
