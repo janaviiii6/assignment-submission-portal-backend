816d67579794f132e15a6fd6a642795d2dc16308
JanaviK <kharatjanavi16@gmail.com>
2024-11-17 11:58:26 +0530

    Setup the basic server with Express and MongoDB connection

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+// Database connection 
+const mongoDBURL = process.env.MONGODB_URL;
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("Connected to Database");
+    })
+    .catch((err) => {
+        console.error("Database connection failed: ", err);
+    });
+
+
+//Listen to the Server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
