799323f39d6a49688807c59ef8834a93bb141bd2
JanaviK <kharatjanavi16@gmail.com>
2024-11-17 22:39:02 +0530

    Integrated function to accept assignment

--- controllers/admin.js
+++ controllers/admin.js
@@ -38,15 +38,61 @@ const viewAllAssignments = async (req,res) => {
         if(assignments.length === 0)
             return res.status(404).json({ message: "No assignments found for this admin" });
 
-        res.status(200).json({
+        return res.status(200).json({
             message: "Assignments retrieved successfully!",
             assignmentDetails,
         });
     }
     catch(err) {
         console.error("Error fetching assignments: ", err);
-        res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error" });
     }
 };
+
+//Function to accept assignment 
+const acceptAssignment = async(req,res) => {
+
+    //Extracting assignment id from the url params
+    const { id } = req.params;
+
+    //Extracting userId from the bearer token
+    const userId = req.user.userId;
+
+    console.log(req.params.id);
+    console.log(userId);
+
+    try{
+        //Find the assignment id
+        const assignment = await Assignment.findById(id);
+
+        //Check if the assignment exists 
+        if(!assignment)
+            return res.status(404).json({ message: "Assignment not found" });
+
+        //Check if the admin in the assignment matches the current user
+        if(assignment.admin.toString() !== userId)
+            return res.status(403).json({ message: "Unauthorized: This assignment is not tagged to you" });
+
+        //Check if the status is already 'accepted'
+        if(assignment.status === 'accepted')
+            return res.status(400).json({ message: "Assignment already accepted by admin" });
+
+        
+        //Update the assignment as accepted
+        assignment.status = "accepted";
+
+        //Save the updated assignment
+        await assignment.save();
+
+        return res.status(200).json({
+            message: "Assignment updated successfully!",
+            assignment,
+        });
+    } 
+    catch(err) {
+        console.error("Error accepting assignment: ", err);
+        return res.status(500).json({ message: "Error accepting assignment. Please try again later." });
+    }
+}
 
 module.exports = { viewAllAssignments, acceptAssignment };
